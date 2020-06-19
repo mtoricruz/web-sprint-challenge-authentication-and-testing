57c0ff8c6934d5148a8a080d8ebf05f14e3936dc
toricruzmendiola <mtoricruz@gmail.com>
2020-06-19 08:49:53 -0700

    implemented authentication workflow with working register/login functions. tested on postman as well. user is unable to view dad joke api unless an account is made, and the user has the right token. after that, they are able to view the list of dad jokes. middleware restrictions work perfectly and now i just need to add a minimum of 2 tests using jests

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,77 @@
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const router = require('express').Router();
+
+const Users = require('../users/users-model');
+const { isValid } = require('../users/users-service');
+const constants = require('../config/constants')
 
 router.post('/register', (req, res) => {
   // implement registration
+  const credentials = req.body;
+
+  if(isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash pass
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save user
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        console.log(user, 'user')
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = createToken(user);
+          res.status(200).json({ message: "You can now view our API", token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password",
+    });
+  }
 });
+
+function createToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role
+  }
+
+  const secret = constants.jwtSecret;
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
+const constants = require('../config/constants')
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  const secret = constants.jwtSecret
+
+  if(token) {
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if(error) {
+        res.status(401).json({ you: 'shall not pass!' });
+      } else {
+        req.decodedToken = decodedToken;
+
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ message: 'please provide credentials to access this resource'})
+  }
 };
